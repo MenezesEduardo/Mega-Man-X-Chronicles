d16bdc67d974b5ac843127d505be1184fc0d4586
Eduardo <eduardu.batista@gmail.com>
2017-12-07 06:28:04 -0200

    Initial commit

---
+++ Assets/Scripts/Robo_amarelo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robo_amarelo : MonoBehaviour {
+
+    public PlayerControler player;
+    public int lifepoints;
+    public Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifepoints <= 0)
+        {
+            anim.SetBool("Destroy", true);
+
+        }
+    }
+
+    public void destruction()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.AddLifePoints(-1);
+        }
+        if (other.gameObject.tag == "Projetil_X_min")
+        {
+            lifepoints = lifepoints - 1;
+        }
+        if (other.gameObject.tag == "Projetil_X_med")
+        {
+            lifepoints = lifepoints - 2;
+        }
+        if (other.gameObject.tag == "Projetil_X_max")
+        {
+            lifepoints = lifepoints - 4;
+        }
+    }
+}
---
+++ Assets/Scripts/Robo_vermelho.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robo_vermelho : MonoBehaviour {
+
+    public PlayerControler player;
+    public int lifepoints;
+    public GameObject proj_position_a1;
+    public GameObject proj_position_bd;
+    public GameObject proj_position_be;
+    public bool cima;
+    public bool baixo;
+    public bool direita;
+    public bool esquerda;
+    public Animator anim;
+    public GameObject proj_enemy_1;
+    public GameObject proj_enemy_2;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lifepoints <= 0)
+        {
+            anim.SetBool("Destroy", true);
+
+        }
+    }
+
+
+    public void destruction()
+    {
+        Destroy(gameObject);
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            player.AddLifePoints(-1);
+        }
+        if (other.gameObject.tag == "Projetil_X_min")
+        {
+            lifepoints = lifepoints - 1;
+        }
+        if (other.gameObject.tag == "Projetil_X_med")
+        {
+            lifepoints = lifepoints - 2;
+        }
+        if (other.gameObject.tag == "Projetil_X_max")
+        {
+            lifepoints = lifepoints - 4;
+        }
+    }
+
+
+    public void attack_central()
+    {
+        if (gameObject.GetComponent<Renderer>().isVisible == true) {
+            if (baixo)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_2, proj_position_a1.transform.position, Quaternion.Euler(new Vector3(0, 0, -90)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.up);
+            }
+            if (cima)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_2, proj_position_a1.transform.position, Quaternion.Euler(new Vector3(0, 0, 90)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.down);
+            }
+            if (direita)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_2, proj_position_a1.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.left);
+
+            }
+            if (esquerda)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_2, proj_position_a1.transform.position, Quaternion.Euler(new Vector3(0, 0, -180)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.right);
+            }
+        }
+ 
+    }
+
+    public void attack_direita()
+    {
+        if (gameObject.GetComponent<Renderer>().isVisible == true)
+        {
+            if (baixo)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_bd.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.right);
+            }
+            if (cima)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_bd.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.left);
+            }
+            if (direita)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_bd.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.up);
+
+            }
+            if (esquerda)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_bd.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.down);
+            }
+        }
+
+    }
+
+
+
+    public void attack_esquerda()
+    {
+        if (gameObject.GetComponent<Renderer>().isVisible == true)
+        {
+            if (baixo)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_be.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.left);
+            }
+            if (cima)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_be.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.right);
+            }
+
+            if (direita)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_be.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.down);
+            }
+            if (esquerda)
+            {
+                GameObject tem = (GameObject)Instantiate(proj_enemy_1, proj_position_be.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                tem.GetComponent<Projetil>().Iniciate(Vector2.up);
+            }
+        }
+    }
+
+}
